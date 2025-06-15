b1f45cdb37f01cd9284076bb3812b3a57fbb4160
Анастасия Рубцова <anastyrub2002@gmail.com>
2025-06-15 19:02:56 +0300

    feat: добавлен компонент BridgingSection, интеграция в main-контент App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Message from './Message';
 import Layout from './components/Layout/Layout';
+import BridgingSection from './components/Section/BridgingSection';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <Layout>
+      <BridgingSection />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
