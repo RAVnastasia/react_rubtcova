580f459d2db50700ff43b0ab4aa1ad1bfa8a321f
Анастасия Рубцова <anastyrub2002@gmail.com>
2025-06-15 18:59:24 +0300

    feat: структура компонентов (Layout, Header, Footer, Main, Navbar, Section, Article), базовые модули стилей, интеграция Layout в App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Message from './Message';
+import Layout from './components/Layout/Layout';
 
 function App() {
   const [count, setCount] = useState(0)
   const messageText = 'Привет из компонента Message!';
 
   return (
-    <>
+    <Layout>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </Layout>
   )
 }
 
